007556236a192f5967dce41312d7a9061dc0cf9d
Olive <golyo278@newschool.edu>
2018-03-15 15:48:35 -0400

    mmm

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function($){
 
 
-$('body').on('keypress', function (a) {   
+$('body').on('keypress', function (a) { 
+console.log('I have been pressed a')  
   $(".a").css("dispay," "block"); 
-  console.log('I have been pressed a');
 });
 
 
