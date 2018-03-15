354d1a3991a8b823fa39b1ddc8abfc36610ca6cd
Olive <golyo278@newschool.edu>
2018-03-15 15:40:46 -0400

    sos

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function($){
+
+var pi= new Array()
+Array= [  "<div class = "a">  <img src="images/a.png" alt="a">  </div>", "<div class = "b">  <img src="images/b.png" alt="b"> </div>", "<div class = "c">  <img src="images/c.png" alt="c"> </div>" ]
+
+
+
+$('body').on('keypress', function (a) {   
+  $(".a").css("dispay," "block"); 
+  console.log('I have been pressed a', a);
+});
+
+
+})
+
+
+
