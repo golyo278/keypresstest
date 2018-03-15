3b4cc131dad8aeba68be601a7ed5ec651bca0fea
Olive <golyo278@newschool.edu>
2018-03-15 15:49:32 -0400

    )

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function($){
 
 
-$('body').on('keypress', function (a) { 
+$('body').on('keypress', function (a)) { 
 console.log('I have been pressed a')  
   $(".a").css("dispay," "block"); 
 });
