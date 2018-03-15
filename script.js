c6f23d482aae54d2499cb414852caf413c9f5ebe
Olive <golyo278@newschool.edu>
2018-03-15 15:43:55 -0400

    ?

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 $(document).ready(function($){
 
-var pi= new Array()
-Array= [  "<div class = "a">  <img src="images/a.png" alt="a">  </div>", "<div class = "b">  <img src="images/b.png" alt="b"> </div>", "<div class = "c">  <img src="images/c.png" alt="c"> </div>" ]
-
 
 
 $('body').on('keypress', function (a) {   
