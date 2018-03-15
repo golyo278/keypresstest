5dccbd77cbe0a235554087c0aa8a1c75889c706c
Olive <golyo278@newschool.edu>
2018-03-15 15:46:29 -0400

    kill me

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function($){
 
 
-
 $('body').on('keypress', function (a) {   
   $(".a").css("dispay," "block"); 
-  console.log('I have been pressed a', a);
+  console.log('I have been pressed a');
 });
 
 
